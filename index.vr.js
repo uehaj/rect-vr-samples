2529ce8959e7d6016fa15bb7b49e21ba280c8433
UEHARA Junji <uehaj@jggug.org>
2017-04-28 15:53:12 +0900

    change 360 image to office

--- index.vr.js
+++ index.vr.js
@@ -8,31 +8,29 @@ import {
   View,
 } from 'react-vr';
 
-const TextPanel = (props) => {
-    return (
-      <Text
-        style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-        }}>
-            hello世界
-        </Text>
-    );
-}
-
 export default class World extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <TextPanel />
+        <Pano source={asset('02-office.jpg')}/>
+        <Text
+          style={{
+            backgroundColor: '#bbaa99',
+            color: 'black',
+            fontSize: 0.4,
+            fontWeight: '700',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.0,
+            paddingRight: 0.0,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [
+              {translate: [-3, 0.2, -6]},
+              {rotateY: 30}
+            ],
+          }}>
+          NTTテクノクロス
+        </Text>
       </View>
     );
   }
